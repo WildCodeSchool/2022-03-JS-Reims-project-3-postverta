cf11e38223ffa1bf5b9100eb9df7739558b063ef
JAAFAR CHERIF <JAAFARCHERIF@gmail.com>
2022-05-31 17:40:53 +0200

    created a User.jsx component and displayed it on App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Home from "./pages/Home";
+import User from "./components/User";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <User />
       <Home />
     </div>
   );
