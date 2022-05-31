6e823093cc29121d35fc12b90ea092a6c807a98a
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-05-31 16:09:39 +0200

    add component Cardlist and make a render in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-import Home from "@pages/Home";
+import Cardlist from "./components/Cardlist";
+import Home from "./pages/Home";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Cardlist />
       <Home />
       <p>coucou</p>
     </div>
