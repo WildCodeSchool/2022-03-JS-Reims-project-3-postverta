00785eb8abb50642d655c9f2019b498158cde86c
JAAFAR CHERIF <JAAFARCHERIF@gmail.com>
2022-05-31 16:10:02 +0200

    created a Userlist.jsx component to make it render a list of users in the App.jsx component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-import Home from "@pages/Home";
+import Home from "./pages/Home";
 
 import "./App.css";
+import Userlist from "./components/Userlist";
 
 function App() {
   return (
     <div className="App">
+      <Userlist />
       <Home />
       <p>coucou</p>
     </div>
---
+++ frontend/src/components/Userlist.jsx
@@ -0,0 +1,48 @@
+const userlist = [
+  {
+    id: "1",
+    name: "John",
+    email: "jonh@mail.com",
+    password: "123456",
+  },
+  {
+    id: "2",
+    name: "Quentin",
+    email: "quentin@mail.com",
+    password: "qspassword",
+  },
+  {
+    id: "3",
+    name: "Louis",
+    email: "louis@mail.com",
+    password: "ldpassword",
+  },
+  {
+    id: "4",
+    name: "Jeffrey",
+    email: "jeffrey@mail.com",
+    password: "jrpassword",
+  },
+  {
+    id: "5",
+    name: "Jaafar",
+    email: "jaafar@mail.com",
+    password: "jcpassword",
+  },
+];
+
+export default function Userlist() {
+  return (
+    <div className="userlist">
+      <ul>
+        {userlist.map((user) => {
+          return (
+            <li key={user.id}>
+              {user.name} {user.email} {user.password}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
