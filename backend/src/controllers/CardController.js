51ad800d0077be105c4093e9e106fd7c0182b25d
PiXeleum <jeffreyrigaux@gmail.com>
2022-05-31 15:21:54 +0200

    add card and user controller

--- backend/src/controllers/CardController.js
+++ backend/src/controllers/CardController.js
@@ -1,8 +1,8 @@
 const models = require("../models");
 
-class ItemController {
+class CardController {
   static browse = (req, res) => {
-    models.item
+    models.card
       .findAll()
       .then(([rows]) => {
         res.send(rows);
@@ -14,7 +14,7 @@ class ItemController {
   };
 
   static read = (req, res) => {
-    models.item
+    models.card
       .find(req.params.id)
       .then(([rows]) => {
         if (rows[0] == null) {
@@ -30,14 +30,14 @@ class ItemController {
   };
 
   static edit = (req, res) => {
-    const item = req.body;
+    const card = req.body;
 
     // TODO validations (length, format...)
 
-    item.id = parseInt(req.params.id, 10);
+    card.id = parseInt(req.params.id, 10);
 
-    models.item
-      .update(item)
+    models.card
+      .update(card)
       .then(([result]) => {
         if (result.affectedRows === 0) {
           res.sendStatus(404);
@@ -52,14 +52,14 @@ class ItemController {
   };
 
   static add = (req, res) => {
-    const item = req.body;
+    const card = req.body;
 
     // TODO validations (length, format...)
 
-    models.item
-      .insert(item)
+    models.card
+      .insert(card)
       .then(([result]) => {
-        res.status(201).send({ ...item, id: result.insertId });
+        res.status(201).send({ ...card, id: result.insertId });
       })
       .catch((err) => {
         console.error(err);
@@ -68,7 +68,7 @@ class ItemController {
   };
 
   static delete = (req, res) => {
-    models.item
+    models.card
       .delete(req.params.id)
       .then(() => {
         res.sendStatus(204);
@@ -80,4 +80,4 @@ class ItemController {
   };
 }
 
-module.exports = ItemController;
+module.exports = CardController;
