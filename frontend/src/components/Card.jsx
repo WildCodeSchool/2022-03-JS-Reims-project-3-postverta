896d72d7a50d4a389422890406fd9dfd50058a10
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-05-31 15:31:32 +0200

    change className for card-name

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -6,7 +6,7 @@ const card = {
 export default function Card() {
   return (
     <div className="card">
-      <h5 className="card-title">{card.name}</h5>
+      <h5 className="card-name">{card.name}</h5>
       <h5 className="card-id">{card.id}</h5>
     </div>
   );
