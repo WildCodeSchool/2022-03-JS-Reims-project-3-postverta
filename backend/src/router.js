3eda4c82ffe5267651dc8e7daa60077d3d759be3
PiXeleum <jeffreyrigaux@gmail.com>
2022-05-31 15:15:21 +0200

    correct 's'

--- backend/src/router.js
+++ backend/src/router.js
@@ -5,16 +5,16 @@ const { ItemController } = require("./controllers");
 const router = express.Router();
 
 router.get("/users", ItemController.browse);
-router.get("/users/:id", ItemController.read);
-router.put("/users/:id", ItemController.edit);
-router.post("/users", ItemController.add);
-router.delete("/users/:id", ItemController.delete);
+router.get("/user/:id", ItemController.read);
+router.put("/user/:id", ItemController.edit);
+router.post("/user", ItemController.add);
+router.delete("/user/:id", ItemController.delete);
 
 
 router.get("/cards", ItemController.browse);
-router.get("/cards/:id", ItemController.read);
-router.put("/cards/:id", ItemController.edit);
-router.post("/cards", ItemController.add);
-router.delete("/cards/:id", ItemController.delete);
+router.get("/card/:id", ItemController.read);
+router.put("/card/:id", ItemController.edit);
+router.post("/card", ItemController.add);
+router.delete("/card/:id", ItemController.delete);
 
 module.exports = router;
