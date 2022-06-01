3cb00d471593813c2c2f9d15fff67f954ede1c12
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-06-01 12:40:57 +0200

    change route in router and resolv eslint error

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,18 +1,19 @@
 const express = require("express");
-const { ItemController } = require("./controllers");
+const { UserController } = require("./controllers");
+const { CardController } = require("./controllers");
 
 const router = express.Router();
 
-router.get("/users", ItemController.browse);
-router.get("/user/:id", ItemController.read);
-router.put("/user/:id", ItemController.edit);
-router.post("/user", ItemController.add);
-router.delete("/user/:id", ItemController.delete);
+router.get("/users", CardController.browse);
+router.get("/user/:id", CardController.read);
+router.put("/user/:id", CardController.edit);
+router.post("/user", CardController.add);
+router.delete("/user/:id", CardController.delete);
 
-router.get("/cards", ItemController.browse);
-router.get("/card/:id", ItemController.read);
-router.put("/card/:id", ItemController.edit);
-router.post("/card", ItemController.add);
-router.delete("/card/:id", ItemController.delete);
+router.get("/cards", UserController.browse);
+router.get("/card/:id", UserController.read);
+router.put("/card/:id", UserController.edit);
+router.post("/card", UserController.add);
+router.delete("/card/:id", UserController.delete);
 
 module.exports = router;
