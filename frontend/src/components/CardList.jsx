2a75298f53a6cee3e4a07a39d295001256ffe49e
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-06-02 16:04:16 +0200

    use crud for call cards and add map Card.jsx

--- frontend/src/components/CardList.jsx
+++ frontend/src/components/CardList.jsx
@@ -1,16 +1,25 @@
-const cardlist = [
-  { id: 1, name: "Card 1" },
-  { id: 2, name: "Card 2" },
-  { id: 3, name: "Card 3" },
-  { id: 4, name: "Card 4" },
-  { id: 5, name: "Card 5" },
-];
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Card from "./Card";
 
 export default function CardList() {
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/cards/")
+      .then((res) => res.data)
+      .then((data) => {
+        setCards(data);
+      });
+  }, []);
+
   return (
     <ul className="cardlist">
-      {cardlist.map((card) => (
-        <li key={card.id}>{card.name}</li>
+      {cards.map((card) => (
+        <li key={card.id}>
+          <Card card={card} />
+        </li>
       ))}
     </ul>
   );
