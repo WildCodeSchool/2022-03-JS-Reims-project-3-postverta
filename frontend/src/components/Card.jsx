734e9e1c30cc558bd69a9a8f45770d9ba2d4a7b4
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-06-02 17:25:14 +0200

    change class to classe

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -3,7 +3,7 @@ import "./Card.css";
 
 export default function Card({ card }) {
   return (
-    <article className={`card ${card.class}`}>
+    <article className={`card ${card.classe}`}>
       <header>
         <h1 className="card__name">{card.name}</h1>
         <span
@@ -20,15 +20,13 @@ export default function Card({ card }) {
           alt="Quentin"
         />
         <figcaption className="card__figcaption">
-          <p className="card__class">
-            [ {card.class} ({card.pv}pv) ]
-          </p>
+          <p className="card__class">[ {card.classe} ]</p>
           <p className="card__type">
             {card.type}/ {card.element}
           </p>
           <p className="card__skill">{card.skill}</p>
           <p className="card__stats">
-            ATK /{card.attack} DEF /{card.defense}
+            ({card.pv}pv) ATK /{card.attack} DEF /{card.defense}
           </p>
         </figcaption>
       </figure>
@@ -40,7 +38,7 @@ Card.propTypes = {
   card: propTypes.shape({
     id: propTypes.number.isRequired,
     name: propTypes.string.isRequired,
-    class: propTypes.string.isRequired,
+    classe: propTypes.string.isRequired,
     pv: propTypes.number,
     type: propTypes.string,
     element: propTypes.string,
