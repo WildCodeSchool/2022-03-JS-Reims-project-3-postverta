f8a1639ff3221b578966c4f91c7b97f3c4f16188
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-06-03 12:24:55 +0200

    add function slugify $

---
+++ frontend/src/services/Slugify.jsx
@@ -0,0 +1,26 @@
+// Slugity a string
+export default function slugify(original) {
+  let slug = original.replace(/^\s+|\s+$/g, "");
+
+  // Make the string lowercase
+  slug = slug.toLowerCase();
+
+  // Remove accents, swap ñ for n, etc
+  const from =
+    "ÁÄÂÀÃÅČÇĆĎÉĚËÈÊẼĔȆÍÌÎÏŇÑÓÖÒÔÕØŘŔŠŤÚŮÜÙÛÝŸŽáäâàãåčçćďéěëèêẽĕȇíìîïňñóöòôõøðřŕšťúůüùûýÿžþÞĐđßÆa·/_,:;";
+  const to =
+    "AAAAAACCCDEEEEEEEEIIIINNOOOOOORRSTUUUUUYYZaaaaaacccdeeeeeeeeiiiinnooooooorrstuuuuuyyzbBDdBAa------";
+  for (let i = 0; i < from.length; i += 1) {
+    slug = slug.replace(new RegExp(from.charAt(i), "g"), to.charAt(i));
+  }
+
+  // Remove invalid chars
+  slug = slug
+    .replace(/[^a-z0-9 -]/g, "")
+    // Collapse whitespace and replace by -
+    .replace(/\s+/g, "-")
+    // Collapse dashes
+    .replace(/-+/g, "-");
+
+  return slug;
+}
