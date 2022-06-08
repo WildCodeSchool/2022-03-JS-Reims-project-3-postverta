de58cf73d5249c74966098c9493287ff847a341b
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-06-08 11:05:26 +0200

    fix eslint error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,5 @@
 import Cardlist from "./components/CardList";
-import UserList from "./components/UserList";
 import Card from "./components/Card";
-import User from "./components/User";
-import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
 import User from "./components/User";
 import UserList from "./components/UserList";
@@ -14,7 +11,6 @@ function App() {
     <div className="App">
       <Cardlist />
       <Card />
-      <Home />
       <User />
       <UserList />
       <NavBar />
