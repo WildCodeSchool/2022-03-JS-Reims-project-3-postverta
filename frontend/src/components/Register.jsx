f14b177deb430274c9e4565e774ccab45c0d9d25
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-06-08 13:31:36 +0200

    add toastify in Register.jsx and send succes promise

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,9 +1,22 @@
 import { useRef } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Register() {
   const nameRef = useRef();
   const emailRef = useRef();
   const passwordRef = useRef();
+  const promise = () => {
+    const resolveafter3sec = new Promise((resolve, reject) => {
+      setTimeout(resolve, 3000);
+      setTimeout(reject, 3000);
+    });
+    toast.promise(resolveafter3sec, {
+      pending: "Inscription en cours ...",
+      success: "Inscription  validé !",
+      error: "Inscription  rejeté !",
+    });
+  };
   return (
     <form
       onSubmit={(event) => {
@@ -22,14 +35,59 @@ export default function Register() {
           },
           body: JSON.stringify(user),
         }).then((response) => {
-          console.warn(response.status);
+          if (response.status === 201) {
+            promise((success) => {
+              toast.success(success);
+            });
+          } else {
+            promise((error) => toast.error(error));
+          }
         });
       }}
     >
-      <input ref={nameRef} type="text" name="name" />
-      <input ref={emailRef} type="text" name="email" />
-      <input ref={passwordRef} type="password" name="password" />
-      <button type="submit">envoyer</button>
+      <div className="m-8">
+        <input
+          ref={nameRef}
+          type="text"
+          name="name"
+          className="w-full h-10 px-3 mb-2 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline"
+          placeholder="Name"
+          required="required"
+        />
+        <input
+          ref={emailRef}
+          type="text"
+          name="email"
+          className="w-full h-10 px-3 mb-2 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline"
+          placeholder="Email"
+          required="required"
+        />
+        <input
+          ref={passwordRef}
+          type="password"
+          name="password"
+          className="w-full h-10 px-3 mb-2 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline"
+          placeholder="Password"
+          required="required"
+        />
+        <button
+          type="submit"
+          className="w-full h-10 px-3 mb-2 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline"
+        >
+          <ToastContainer
+            position="top-center"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+          Register
+        </button>
+      </div>
     </form>
   );
 }
