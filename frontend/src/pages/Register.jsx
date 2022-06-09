05466723d6c2891d1186b4d9278f3e2b48e21a42
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-06-09 16:02:51 +0200

    add loginform

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,11 @@
 import { Link } from "react-router-dom";
+import LoginForm from "../components/LoginForm";
 
 export default function Register() {
-  return <Link to="/account">Compte utilisateur</Link>;
+  return (
+    <>
+      <Link to="/account">Compte utilisateur</Link>
+      <LoginForm />
+    </>
+  );
 }
