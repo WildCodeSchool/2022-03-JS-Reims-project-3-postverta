3b501078ba94ad0ab21dfa3386782f4f7dff10db
JAAFAR CHERIF <JAAFARCHERIF@gmail.com>
2022-06-09 10:11:14 +0200

    renamed Register component to RegisterForm and added style and form to Register page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,12 @@
 import { Link } from "react-router-dom";
+import RegisterForm from "../components/RegisterForm";
 
 export default function Register() {
-  return <Link to="/account">Compte utilisateur</Link>;
+  return (
+    <div className="bg-black text-white">
+      <img src="../../src/assets/img/logo-black.jpg" alt="logo" />
+      <Link to="/account">Compte utilisateur</Link>
+      <RegisterForm />
+    </div>
+  );
 }
