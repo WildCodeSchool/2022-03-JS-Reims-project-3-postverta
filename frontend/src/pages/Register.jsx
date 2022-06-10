f50f1941026c07041a578e5ef24660d6aaed71a8
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-06-10 09:35:36 +0200

    rename logo variable

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
 import RegisterForm from "../components/RegisterForm";
 import LoginForm from "../components/LoginForm";
-import blacklogo from "../assets/img/logo-black.jpg";
+import logoBlack from "../assets/img/logo-black.jpg";
 
 export default function Register() {
   return (
     <div className="bg-black text-white">
-      <img src={blacklogo} alt="logo" />
+      <img src={logoBlack} alt="logo" />
       <Link to="/account">Compte utilisateur</Link>
       <RegisterForm />
       <LoginForm />
