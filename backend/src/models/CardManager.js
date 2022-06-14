fbe5e44b22bc177767f884a33c254085e79fc30a
loulou0704 <louis51@hotmail.fr>
2022-06-14 16:25:10 +0200

    Created a function findByUser

--- backend/src/models/CardManager.js
+++ backend/src/models/CardManager.js
@@ -3,6 +3,13 @@ const AbstractManager = require("./AbstractManager");
 class CardManager extends AbstractManager {
   static table = "card";
 
+  findByUser(userId) {
+    return this.connection.query(
+      `select * from  ${CardManager.table} inner join card_user on card.id = card_user.card_id where user_id = ?`,
+      [userId]
+    );
+  }
+
   insert(card) {
     return this.connection.query(
       `insert into ${CardManager.table} (name) values (?)`,
