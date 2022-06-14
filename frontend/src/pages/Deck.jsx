32cd00f059138410b503a3d5ac7975cf0476137a
JAAFAR CHERIF <JAAFARCHERIF@gmail.com>
2022-06-14 11:15:59 +0200

    added background to Deck and Library pages

--- frontend/src/pages/Deck.jsx
+++ frontend/src/pages/Deck.jsx
@@ -1,5 +1,10 @@
 import CardList from "../components/CardList";
 
 export default function Deck() {
-  return <CardList />;
+  return (
+    <div className="bg-[url('./assets/img/dream-deck.jpg')] bg-repeat">
+      <p>Deck</p>
+      <CardList />
+    </div>
+  );
 }
--- frontend/src/pages/Library.jsx
+++ frontend/src/pages/Library.jsx
@@ -1,3 +1,8 @@
 export default function Library() {
-  return <p>Bibliothèque</p>;
+  return (
+    <div className="bg-[url('./assets/img/dream-library.jpg')] min-h-screen bg-no-repeat bg-cover">
+      <p>Bibliothèque</p>
+      <p>Exemple de livre</p>
+    </div>
+  );
 }
