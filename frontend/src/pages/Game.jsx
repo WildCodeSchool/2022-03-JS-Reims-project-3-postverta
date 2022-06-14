8e71dd6ab6c234dd335d5d25ffb71b6bdebca7dc
JAAFAR CHERIF <JAAFARCHERIF@gmail.com>
2022-06-14 11:04:05 +0200

    added background images to tailwind.config file and added style to the Game page using one of these images

--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -1,3 +1,8 @@
 export default function Game() {
-  return <p>Jeu</p>;
+  return (
+    <div className="bg-[url('./assets/img/dream-game.jpg')] min-h-screen bg-no-repeat bg-cover">
+      <p>Jeu</p>
+      <p>Carte</p>
+    </div>
+  );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "account-bg": 'url("/assets/img/dream-account.jpg")',
+        "game-bg": 'url("/assets/img/dream-game.jpg")',
+        "deck-bg": 'url("/assets/img/dream-deck.jpg")',
+        "library-bg": 'url("/assets/img/dream-library.jpg")',
+      },
+    },
   },
   plugins: [],
 };
