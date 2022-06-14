ba6663e4220863c2ae48175aae6b5c5957c0b7e8
JAAFAR CHERIF <JAAFARCHERIF@gmail.com>
2022-06-14 12:36:15 +0200

    removed paths from tailwind.config file

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,7 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        "account-bg": 'url("/assets/img/dream-account.jpg")',
-        "game-bg": 'url("/assets/img/dream-game.jpg")',
-        "deck-bg": 'url("/assets/img/dream-deck.jpg")',
-        "library-bg": 'url("/assets/img/dream-library.jpg")',
-      },
-    },
+    extend: {},
+    plugins: [],
   },
-  plugins: [],
 };
