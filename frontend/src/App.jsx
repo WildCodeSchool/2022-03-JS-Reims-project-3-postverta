d79188a51cb3801a8fc204a6521dfe103c3f8026
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-06-14 11:18:52 +0200

    make new route for home  register and login pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 import Library from "./pages/Library";
 import Game from "./pages/Game";
 import Deck from "./pages/Deck";
@@ -12,7 +14,9 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Register />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/library" element={<Library />} />
           <Route path="/game" element={<Game />} />
           <Route path="/deck" element={<Deck />} />
