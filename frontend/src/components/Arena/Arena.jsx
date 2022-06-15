28756f30eb188a15dc572a6b02ca6c8c7725e14f
PiXeleum <jeffreyrigaux@gmail.com>
2022-06-15 17:14:33 +0200

    add tempory hand syqtem

--- frontend/src/components/Arena/Arena.jsx
+++ frontend/src/components/Arena/Arena.jsx
@@ -5,7 +5,15 @@ export default function Arena() {
     <>
       <div className="advHand h-1/4">advHand</div>
       <div className="ground h-2/4 ">ground</div>
-      <Hand />
+      <details className="h-1/4">
+        <summary>
+          <img
+            src="https://www.ncbi.ie/wp-content/uploads/2020/12/DL0020a-600x702.jpg"
+            alt="cards"
+          />
+        </summary>
+        <Hand />
+      </details>
     </>
   );
 }
