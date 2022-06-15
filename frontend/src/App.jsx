fa2c03ef127cea2dc5550a75d446e15b4f30e203
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-06-15 12:09:42 +0200

    use userdataprovider in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { UserDataProvider } from "./context/UserDataContext";
 import Register from "./pages/Register";
 import Library from "./pages/Library";
 import Game from "./pages/Game";
@@ -10,16 +11,18 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Register />} />
-          <Route path="/library" element={<Library />} />
-          <Route path="/game" element={<Game />} />
-          <Route path="/deck" element={<Deck />} />
-          <Route path="/account" element={<Account />} />
-        </Routes>
-        <Navbar />
-      </Router>
+      <UserDataProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Register />} />
+            <Route path="/library" element={<Library />} />
+            <Route path="/game" element={<Game />} />
+            <Route path="/deck" element={<Deck />} />
+            <Route path="/account" element={<Account />} />
+          </Routes>
+          <Navbar />
+        </Router>
+      </UserDataProvider>
     </div>
   );
 }
