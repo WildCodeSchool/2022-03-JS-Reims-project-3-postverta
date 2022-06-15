da8412a633842069e762db1ccb06dc287470ad3d
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-06-15 12:08:54 +0200

    make a render for userdata on account page

--- frontend/src/pages/Account.jsx
+++ frontend/src/pages/Account.jsx
@@ -1,3 +1,15 @@
+import { useUserData } from "../context/UserDataContext";
+
 export default function Account() {
-  return <p>Compte utilisateur</p>;
+  const { userData } = useUserData();
+
+  return (
+    <>
+      <p> Nom : {userData.name}</p>
+      <p>Mail : {userData.email}</p>
+      <p>Pseudo : {userData.pseudo}</p>
+      <p>Date de naissance : {userData.birthdate}</p>
+      <p>Genre : {userData.gender}</p>
+    </>
+  );
 }
