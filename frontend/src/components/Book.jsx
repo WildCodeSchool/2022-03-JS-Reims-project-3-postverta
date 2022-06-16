46e6c8c446d046e3db3104653d73d1657048b081
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-06-16 16:51:01 +0200

    add style and change css for button

--- frontend/src/components/Book.jsx
+++ frontend/src/components/Book.jsx
@@ -17,16 +17,16 @@ export default function Book({ book }) {
       <figure className={`flip-card ${isFlipped ? "hover" : ""}`}>
         <img className="flip-card-front" src={book.cover} alt={book.title} />
         <figcaption className="flip-card-back">
-          <h1>{book.title}</h1>
-          <p>{book.synopsis}</p>
+          <h1 className="m-2">{book.title}</h1>
+          <p className="m-5">{book.synopsis}</p>
         </figcaption>
       </figure>
       <button
         type="button"
-        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+        className="bg-white font-semibold py-2 px-4 border border-black rounded-3xl mt-4"
         onClick={toggleFlip}
       >
-        Flip
+        Synopsis
       </button>
     </div>
   );
