c18e19d249724595bc981497cc5084c4fdc04fa6
loulou0704 <louis51@hotmail.fr>
2022-06-17 09:26:20 +0200

    in the CardUsermanager.js file I created an updateCard function and a deleteCard function.

--- backend/src/models/CardUserManager.js
+++ backend/src/models/CardUserManager.js
@@ -1,13 +1,26 @@
 const AbstractManager = require("./AbstractManager");
 
 class CardUserManager extends AbstractManager {
-  static table = "carduser";
+  static table = "card_user";
 
-  insertCard(userId, cardId) {
+  insertCard(cardId, userId) {
     return this.connection.query(
       `insert into card_user (user_id, card_id) values (?, ?)`,
       [userId, cardId]
     );
+  }
+
+  updateCard(cardId, userId, active) {
+    return this.connection.query(
+      `update card_user set active = ? where card_id = ? and user_id = ?`,
+      [active, cardId, userId]
+    );
+  }
+
+  deleteCard(cardId) {
+    return this.connection.query(`delete from card_user where card_id = ?`, [
+      cardId,
+    ]);
   }
 }
 
