dbf5714e99ec79b6b54e86d2925fec510c02b44a
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-06-22 15:23:13 +0200

    call Myground ,TurnButton and OppenentGround in Arena

--- frontend/src/components/Arena/Arena.jsx
+++ frontend/src/components/Arena/Arena.jsx
@@ -1,11 +1,19 @@
-import Hand from "./Hand";
+import MyGround from "./Ground/MyGround";
+import OppenentGround from "./Ground/OppenentGround";
+import TurnButton from "./Ground/TurnButton";
 
 export default function Arena() {
   return (
-    <>
-      <div className="advHand h-1/4">advHand</div>
-      <div className="ground h-2/4 ">ground</div>
-      <Hand />
-    </>
+    <div className=" min-h-screen flex flex-col justify-between">
+      <div className="h-32 border-2" />
+      <div className="h-96 m-2">
+        <div className="-rotate-180">
+          <OppenentGround />
+        </div>
+        <TurnButton />
+        <MyGround />
+      </div>
+      <div className="h-32 border-2" />
+    </div>
   );
 }
