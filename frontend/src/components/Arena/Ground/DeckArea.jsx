79edf18908d82b1b2c1ab3d1cf00ee9ee4fb17ad
PiXeleum <jeffreyrigaux@gmail.com>
2022-06-23 15:35:33 +0200

    add comm to DeckArea

--- frontend/src/components/Arena/Ground/DeckArea.jsx
+++ frontend/src/components/Arena/Ground/DeckArea.jsx
@@ -3,7 +3,9 @@ import ReverseCard from "../../../assets/img/Dos-des-cartes.png";
 export default function DeckArea() {
   return (
     <div className="grid grid-cols-1 justify-items-center place-content-center gap-12 mr-14">
+      {/* defausse */}
       <div className="border-2 border-black h-14 w-10   " />
+      {/* deck */}
       <img
         alt="ReverseCard"
         src={ReverseCard}
