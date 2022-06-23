2aff1a347e824e6028327bb863df18530b934623
PiXeleum <jeffreyrigaux@gmail.com>
2022-06-23 12:36:08 +0200

    add picture deck

--- frontend/src/components/Arena/Ground/DeckArea.jsx
+++ frontend/src/components/Arena/Ground/DeckArea.jsx
@@ -1,8 +1,14 @@
+import ReverseCard from "../../../assets/img/Dos-des-cartes.png";
+
 export default function DeckArea() {
   return (
     <div className="grid grid-cols-1 justify-items-center place-content-center gap-12 mr-14">
       <div className="border-2 border-black h-14 w-10   " />
-      <div className="border-2 border-black h-14 w-10 bg-black" />
+      <img
+        alt="ReverseCard"
+        src={ReverseCard}
+        className="border-2 border-black h-14 w-10 bg-black"
+      />
     </div>
   );
 }
