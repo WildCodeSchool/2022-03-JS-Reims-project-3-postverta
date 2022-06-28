af0c0ef8aff966b4affbc339aa085b5709850e01
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-06-28 17:06:09 +0200

    make function for show hand and for pick a card

--- frontend/src/components/Arena/Arena.jsx
+++ frontend/src/components/Arena/Arena.jsx
@@ -1,9 +1,46 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Hand from "./Hand";
 import OppenentCard from "./OppenentCard";
 import TurnButton from "./Ground/TurnButton";
 import Ground from "./Ground/Ground";
 
+const deck = [1, 31, 26, 21, 23, 11, 3, 7, 10, 12, 28, 30];
+const shuffleDeck = deck.sort(() => Math.random() - 0.5);
+const pickCardId = () => shuffleDeck.shift();
+
+const createHand = (cards) => {
+  const hand = [];
+  for (let i = 0; i < 5; i += 1) {
+    hand.push(cards.shift());
+  }
+  return hand;
+};
+
+const initialHandIds = createHand(shuffleDeck);
+
 export default function Arena() {
+  const [hand, setHand] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/cards")
+      .then((res) => res.data)
+      .then((cards) => {
+        setHand(cards.filter((card) => initialHandIds.includes(card.id)));
+      });
+  }, []);
+
+  const drawCard = () => {
+    const cardId = pickCardId();
+    axios
+      .get(`http://localhost:5000/cards/${cardId}`)
+      .then((res) => res.data)
+      .then((card) => {
+        setHand([...hand, card]);
+      });
+  };
+
   return (
     <div className=" min-h-screen flex flex-col justify-between">
       <OppenentCard />
@@ -11,10 +48,17 @@ export default function Arena() {
         <div className="-rotate-180">
           <Ground />
         </div>
+        <button
+          type="button"
+          className="bg-black justify-end text-white font-bold py-1 px-2 m-3 rounded-full"
+          onClick={drawCard}
+        >
+          Piocher
+        </button>
         <TurnButton />
         <Ground />
       </div>
-      <Hand />
+      <Hand hand={hand} />
     </div>
   );
 }
