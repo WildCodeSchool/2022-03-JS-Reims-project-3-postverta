4db11014e7b9fbb35150e53a2f056ee658a0980d
JAAFAR CHERIF <JAAFARCHERIF@gmail.com>
2022-07-06 17:52:34 +0200

    added a modal with its css

--- frontend/src/components/Arena/Hand.jsx
+++ frontend/src/components/Arena/Hand.jsx
@@ -1,19 +1,42 @@
 import propTypes from "prop-types";
 import Card from "../Card";
+import "../Modal.css";
+
+function Modal({ children }) {
+  return <details className="modal">{children}</details>;
+}
+
+function Trigger({ children }) {
+  return <summary className="modal-trigger">{children}</summary>;
+}
+
+Modal.Trigger = Trigger;
+
+function Window({ children }) {
+  return <div className="modal-window">{children}</div>;
+}
+
+Modal.Window = Window;
 
 export default function Hand({ hand, playCard }) {
   return (
     <div className=" h-1/4 flex  overflow-x-auto ">
       {hand.map((card) => (
-        <button
-          key={card.id}
-          onClick={() => playCard(card.id)}
-          type="button"
-          className="border-2 m-2 mr-8
+        <Modal>
+          <Modal.Trigger
+            key={card.id}
+            className="border-2 m-2 mr-8
          h-max text-xs"
-        >
-          <Card key={card.id} card={card} />
-        </button>
+          >
+            <Card key={card.id} card={card} />
+          </Modal.Trigger>
+          <Modal.Window>
+            <Card key={card.id} card={card} className="card" />
+            <button onClick={() => playCard(card.id)} type="button">
+              poser
+            </button>
+          </Modal.Window>
+        </Modal>
       ))}
     </div>
   );
@@ -35,4 +58,16 @@ Hand.propTypes = {
     })
   ).isRequired,
   playCard: propTypes.func.isRequired,
+};
+
+Modal.propTypes = {
+  children: propTypes.node.isRequired,
+};
+
+Trigger.propTypes = {
+  children: propTypes.node.isRequired,
+};
+
+Window.propTypes = {
+  children: propTypes.node.isRequired,
 };
