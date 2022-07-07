bf78eca02454adb6462b881242dace760550f9df
QuentinSamuel <quentinsamuelpro@gmail.com>
2022-07-07 10:29:30 +0200

    change content of graveyard for have just one div

--- frontend/src/components/Arena/Ground/GraveyardArea.jsx
+++ frontend/src/components/Arena/Ground/GraveyardArea.jsx
@@ -1,8 +1,3 @@
 export default function GraveyardArea() {
-  return (
-    <div className=" grid grid-cols-1 justify-items-center place-content-center gap-16 ">
-      <div className="border-2 border-black h-20 w-14 md:h-32 md:w-24" />
-      <div className="border-2 border-black h-20 w-14 md:h-32 md:w-24" />
-    </div>
-  );
+  return <div className="border-2 border-black h-20 w-16 md:h-32 md:w-24" />;
 }
