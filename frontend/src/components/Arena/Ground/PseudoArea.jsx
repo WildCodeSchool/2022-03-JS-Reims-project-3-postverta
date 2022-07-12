7fe616a104e935302d7d4dab0937898e3eeb6085
loulou0704 <louis51@hotmail.fr>
2022-07-12 17:13:33 +0200

    imported useUserData in the PseudoArea.jsx file

--- frontend/src/components/Arena/Ground/PseudoArea.jsx
+++ frontend/src/components/Arena/Ground/PseudoArea.jsx
@@ -1,7 +1,10 @@
+import { useUserData } from "../../../context/UserDataContext";
+
 export default function PseudoArea() {
+  const { userData } = useUserData();
   return (
     <div className="  flex justify-items-center place-content-center gap-2 text-lg text-white font-bold">
-      <p>pseudo :</p>
+      <p>{userData.pseudo} :</p>
       <p>40 pv </p>
     </div>
   );
