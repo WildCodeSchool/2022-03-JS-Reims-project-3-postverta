644ee152b2c04a631708f6e3704cd0ff9b445d01
loulou0704 <louis51@hotmail.fr>
2022-07-12 17:11:41 +0200

    created an Opponenttseudo component

---
+++ frontend/src/components/Arena/Ground/OpponentPseudo.jsx
@@ -0,0 +1,8 @@
+export default function OpponentPseudo() {
+  return (
+    <div className="  flex justify-items-center place-content-center gap-2 text-lg text-white font-bold">
+      <p>Nibor :</p>
+      <p>40 pv </p>
+    </div>
+  );
+}
