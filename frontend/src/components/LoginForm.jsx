8ad90f70cec6485a85206571e57ea92525ae822f
JAAFAR CHERIF <JAAFARCHERIF@gmail.com>
2022-07-26 17:18:59 +0200

    deleted app class in index.css and put a h-screen on loginform, registerform and home so the navbar doesn't move between pages

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -13,7 +13,7 @@ export default function LoginForm() {
 
   return (
     <form
-      className="p-8 bg-black text-white flex flex-col justify-center items-center font-['Sansita-Regular']"
+      className="p-8 bg-black text-white flex flex-col h-screen justify-center items-center font-['Sansita-Regular']"
       onSubmit={(event) => {
         event.preventDefault();
         const login = {
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import logoBlack from "../assets/img/logo-black.jpg";
 
 export default function Home() {
   return (
-    <div className="font-['Sansita-Regular'] bg-black text-white flex flex-col justify-center items-center ">
+    <div className="font-['Sansita-Regular'] bg-black text-white flex flex-col h-screen justify-center items-center ">
       <h1 className="text-7xl  ">Postverta</h1>
       <img src={logoBlack} alt="logo" className="m-3 w-80" />
 
